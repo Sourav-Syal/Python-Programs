19c32e164e9b63c305ad0cebc9db06ff62a8ca34
Sourav-Syal <souravsyal0503@gmail.com>
2025-02-16 23:09:24 +0530

    Removed programs

--- Strings&Lists/Userdataformat.py
+++
@@ -1,9 +0,0 @@
-counter = int(input("how many fruits you want to add?"))
-
-str1 = ""
-for _ in range(counter):
-    userinput = input("Enter name of fruits:")
-    str1 = str1 + userinput + " "
-
-print(", ".join(str1.split()))
-
